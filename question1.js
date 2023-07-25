c49ff07fa9e18b569423a6522031b17952535951
ds asante <114578080+dsasante1@users.noreply.github.com>
2023-07-25 15:07:44 +0000

    format code

--- question1.js
+++ question1.js
@@ -1,38 +1,30 @@
-function checkNumbers(testNumber){
-
-    if (typeof testNumber === 'number' && !Number.isNaN(testNumber)){
-        return true
-    }
-    return false
-
+function checkNumbers (testNumber) {
+  if (typeof testNumber === 'number' && !Number.isNaN(testNumber)) {
+    return true;
+  }
+  return false;
 }
 
+function fizzBuzz (n) {
+  const result = [];
 
-
-
-
-function fizzBuzz (n){
-
-    let result = []
-
-    if (checkNumbers(n)){
-
-        for (let counter = 1; counter <= n; counter += 1){
-            if (counter % 3 === 0 && counter % 5 === 0){
-                result.push("FizzBuzz");
-            }else if (counter % 3 === 0){
-                result.push("Fizz")
-            }else if(counter % 5 === 0){
-                result.push("Buzz")
-            }else{
-                result.push(counter.toString())
-            }
-        }
-
-        return result
-    }else{
-        return 'invalid input! enter a number'
+  if (checkNumbers(n)) {
+    for (let counter = 1; counter <= n; counter += 1) {
+      if (counter % 3 === 0 && counter % 5 === 0) {
+        result.push('FizzBuzz');
+      } else if (counter % 3 === 0) {
+        result.push('Fizz');
+      } else if (counter % 5 === 0) {
+        result.push('Buzz');
+      } else {
+        result.push(counter.toString());
+      }
     }
+
+    return result;
+  } else {
+    return 'invalid input! enter a number';
+  }
 }
 
 module.exports = fizzBuzz;
--- test.js
+++ test.js
@@ -1,21 +1,15 @@
 
 const fizzBuzz = require('./question1.js');
 
-
 test('fizz buzz input 3', () => {
-    expect(fizzBuzz(3)).toStrictEqual(["1","2","Fizz"]);
-   
-  });
-
-
-  test('fizz buzz input 5', () => {
-  expect(fizzBuzz(5)).toStrictEqual(["1","2","Fizz","4","Buzz"]);
+  expect(fizzBuzz(3)).toStrictEqual(['1', '2', 'Fizz']);
 });
 
-
+test('fizz buzz input 5', () => {
+  expect(fizzBuzz(5)).toStrictEqual(['1', '2', 'Fizz', '4', 'Buzz']);
+});
 
 test('fizz buzz input 15', () => {
-  expect(fizzBuzz(15)).toStrictEqual(["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz",
-  "13","14","FizzBuzz"]);
-
+  expect(fizzBuzz(15)).toStrictEqual(['1', '2', 'Fizz', '4', 'Buzz', 'Fizz', '7', '8', 'Fizz', 'Buzz', '11', 'Fizz',
+    '13', '14', 'FizzBuzz']);
 });
