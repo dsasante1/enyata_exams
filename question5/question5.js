bf0b1a8d746294f57aeddd52e854ac3bc52982a9
ds asante <114578080+dsasante1@users.noreply.github.com>
2023-07-28 00:21:30 +0000

    done: question 5 test
    changed test from jest to mocha. fixed: issue was import and export js function modules

--- question5/question5.js
+++ question5/question5.js
@@ -1,5 +1,5 @@
-// Given an unsorted array of numbers, write two functions. One should sort the array in
-// ascending order and the other should sort the array in descending order.
+// // Given an unsorted array of numbers, write two functions. One should sort the array in
+// // ascending order and the other should sort the array in descending order.
 
 function checkNumbers (arrays) {
     let result = []
@@ -12,32 +12,35 @@ function checkNumbers (arrays) {
   return result;
 }
 
+//
+let arrayItem1 = [1, 4, 3, 2, 5]
+let arrayItem2 = [1, 4, 3, 2, 5]
 
-let arrayItem = [1, 4, 3, 2,5]
 
-
-function sortAscendingOrder(arrayItem){
-    if (Array.isArray(arrayItem)) {
-        let newArray = checkNumbers(arrayItem)
+function sortAscendingOrder(arrayItem1){
+    if (Array.isArray(arrayItem1)) {
+        let newArray = checkNumbers(arrayItem1)
         return newArray.sort()
     }else{
         return 'invalid input. enter an array of numbers'
     }
 }
 
-function sortDescendingOrder(arrayItem){
-     if (Array.isArray(arrayItem)) {
-        let newArray = checkNumbers(arrayItem)
+function sortDescendingOrder(arrayItem2){
+     if (Array.isArray(arrayItem2)) {
+        let newArray = checkNumbers(arrayItem2)
         return newArray.sort().reverse()
     }else{
         return 'invalid input. enter an array of numbers'
     }
 
 }
+//
+//
+console.log(sortAscendingOrder(arrayItem1))
+console.log(sortDescendingOrder(arrayItem2))
+//
+exports.sortAscendingOrder = sortAscendingOrder;
+exports.sortDescendingOrder = sortDescendingOrder;
 
 
-console.log(sortAscendingOrder(arrayItem))
-console.log(sortDescendingOrder(arrayItem))
-
-module.exports = sortAscendingOrder;
-module.exports = sortDescendingOrder;
--- question5/test.js
+++ question5/test.js
@@ -1,14 +1,23 @@
-const sortAscendingOrder = require('./question5.js');
-const sortDescendingOrder = require('./question5.js');
+const { describe } = require('mocha');
+const { sortAscendingOrder, sortDescendingOrder } = require('./question5.js');
+// const sortDescendingOrder = require('./question5.js');
+const assert = require("assert");
 
-test('sort unordered array in ascending order', () => {
-  expect(sortAscendingOrder([3, 4, 2, 5, 1])).toStrictEqual([ 1, 2, 3, 4, 5 ]);
+// test('sort unordered array in ascending order', () => {
+//   expect(sortAscendingOrder([3, 4, 2, 5, 1])).toStrictEqual([ 1, 2, 3, 4, 5 ]);
 
-});
+// });
+
+describe("test the ascending order function", function (){
+  it("sort an unordered array to ascending order", function (){
+    assert.deepStrictEqual(sortAscendingOrder([3, 4, 2, 5, 1]), [1, 2, 3, 4, 5]);
+  }),
+
+    it("sort an unordered array to descending order", function (){
+    assert.deepStrictEqual(sortDescendingOrder([3, 4, 2, 5, 1]), [5, 4, 3, 2, 1]);
+  })
 
 
-test('sort unordered array in descending order', () => {
-  expect(sortDescendingOrder([1, 4, 3, 2,5])).toStrictEqual([5, 4, 3, 2, 1]);
 
-});
+})
 
